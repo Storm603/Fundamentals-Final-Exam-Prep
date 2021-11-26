5072aa813dde4763e57659eda8b92c2a7f38bdde
Storm603 <93555141+Storm603@users.noreply.github.com>
2021-11-26 23:48:57 +0200

    Add files via upload

---
+++ T01. World Tour/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace T01._World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder desinations = new StringBuilder(Console.ReadLine());
+
+            string[] command = Console.ReadLine().Split(":");
+
+            while (command[0] != "Travel")
+            {
+                string action = command[0];
+
+                if (action == "Add Stop")
+                {
+                    int index = int.Parse(command[1]);
+                    string substring = command[2];
+
+                    if (desinations.Length > index && index >= 0)
+                    {
+                        desinations.Insert(index, substring);
+                    }
+                    Console.WriteLine(desinations);
+                }
+                else if (action == "Remove Stop")
+                {
+                    int startIndex = int.Parse(command[1]);
+                    int endIndex = int.Parse(command[2]);
+
+                    int length = (endIndex+1) - startIndex;
+
+                    if (startIndex >= 0 && startIndex < desinations.Length)
+                    {
+                        if (endIndex >= 0 && endIndex < desinations.Length)
+                        {
+                            desinations.Remove(startIndex, length);
+                        }
+                    }
+                    Console.WriteLine(desinations);
+                }
+                else if (action == "Switch")
+                {
+                    string oldSubstring = command[1];
+                    string newSubstring = command[2];
+
+                    string container = desinations.ToString();
+
+                    if (container.Contains(oldSubstring))
+                    {
+                        desinations.Replace(oldSubstring, newSubstring);
+                    }
+                    Console.WriteLine(desinations);
+                }
+                command = Console.ReadLine().Split(":");
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {desinations}");
+
+        }
+    }
+}
---
+++ Т03. Plant Discovery/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Т03._Plant_Discovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            Dictionary<string, double> plants = new Dictionary<string, double>();  // name - rarity 
+            Dictionary<string, List<double>> averageRating = new Dictionary<string, List<double>>(); // name - rating
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] info = Console.ReadLine().Split("<->");
+
+                string plantName = info[0];
+                double plantRarity = double.Parse(info[1]);
+
+                int plantRating = 0;
+
+                if (!plants.ContainsKey(plantName))
+                {
+                    plants.Add(plantName, plantRarity);
+
+                    averageRating.Add(plantName, new List<double>());
+                    averageRating[plantName].Add(plantRating);
+                }
+                else if(plants.ContainsKey(info[0]))
+                {
+                    plants[plantName] = plantRarity;
+                }
+                
+            }
+
+            string[] command = Console.ReadLine().Split(" - ").ToArray();
+            string[] splitCmd1 = command[0].Split(": ").ToArray();
+
+            while (splitCmd1[0] != "Exhibition")
+            {
+                string action = splitCmd1[0];
+                string plant = splitCmd1[1];
+
+                if (!plants.ContainsKey(plant)) // removed  || !averageRating.ContainsKey(plant)
+                {
+                    Console.WriteLine("error");
+                }
+                else if (action == "Rate")
+                {
+                    double rating = double.Parse(command[1]);
+                    averageRating[plant].Add(rating);
+                }
+                else if (action == "Update")
+                {
+                    double plantRarity = double.Parse(command[1]);
+                    plants[plant] = plantRarity;
+                }
+                else if (action == "Reset")
+                {
+                    averageRating[plant].Clear();
+                    averageRating[plant].Add(0);
+                }
+
+                command = Console.ReadLine().Split(" - ").ToArray(); 
+                splitCmd1 = command[0].Split(": ").ToArray();
+            }
+
+            Dictionary<string, List<double>> final = new Dictionary<string, List<double>>(); // name - rarity - rating
+
+            foreach (var rate in averageRating)
+            {
+                string plantName = rate.Key;
+                double rarity = plants[plantName];
+
+                if (rate.Value.Count == 1 && rate.Value.Contains(0))
+                {
+                    final.Add(plantName, new List<double>());
+                    final[plantName].Add(rarity);
+                    final[plantName].Add(0);
+                }
+                else
+                {
+                    rate.Value.Remove(0);
+                    //double average = rate.Value.Average();
+                    final.Add(plantName, new List<double>());
+                    final[plantName].Add(rarity);
+                    final[plantName].Add(rate.Value.Average());
+                }
+            }
+
+            final = final.OrderByDescending(x => x.Value[0]).ThenByDescending(x => x.Value[1]).ToDictionary(x => x.Key, x=> x.Value);
+
+            Console.WriteLine("Plants for the exhibition:");
+
+            foreach (var plant in final)
+            {
+                Console.WriteLine($"- {plant.Key}; Rarity: {plant.Value[0]}; Rating: {plant.Value[1]:f2}");
+            }
+        }
+    }
+}
